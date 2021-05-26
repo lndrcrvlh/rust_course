6f016463e90acbc7966535e498c276c0725a23e5
leo <lndrcrvlh@gmail.com>
2021-05-26 09:16:26 -0300

    Bitwise operators lesson

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,44 @@
 fn main() {
-    println!("Hello, world!");
+    
+    let mut value = 0b1111_0101u8;
+
+    println!("Value is {}", value);
+    println!("the value is {:08b}", value);
+
+    value = !value;
+
+    println!("Value is {}", value);
+    println!("the value is {:08b}", value);
+
+    value = value & 0b1111_0111;
+
+    println!("Value is {}", value);
+    println!("the value is {:08b}", value);
+
+    println!("bit 6 is {}", value & 0b0100_0000);
+
+    value = value | 0b0100_0000;
+    println!("Value is {}", value);
+    println!("the value is {:08b}", value);
+
+    println!("bit 6 is {}", value & 0b0100_0000);
+
+    value = value ^ 0b0101_0101;
+    println!("Value is {}", value);
+    println!("the value is {:08b}", value);
+    
+    value = value << 4;
+    println!("Value after left shift 4 times {}", value);
+    println!("the value is {:08b}", value);
+
+    value = value >> 2;
+    println!("Value after right shift 2 times {}", value);
+    println!("the value is {:08b}", value);
+
+    value = value >> 2;
+    println!("Value after right shift 2 more times {}", value);
+    println!("the value is {:08b}", value);
+    value = value >> 2;
+    println!("Value after right shift 2 more times {}", value);
+    println!("the value is {:08b}", value);
 }
