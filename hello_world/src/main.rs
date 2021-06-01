772002a953cfa1b3558a4cc9f6da2cbde175c868
leo <lndrcrvlh@gmail.com>
2021-06-01 15:21:37 -0300

    Celsius to farenheit challenge solution

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,44 +1,12 @@
 fn main() {
-    
-    let mut value = 0b1111_0101u8;
+    let celsius_temp = 23.0;
+    let farenheit_temp = celsius_to_farenheit(celsius_temp);
 
-    println!("Value is {}", value);
-    println!("the value is {:08b}", value);
+    assert_eq!(farenheit_temp,73.4);
+    println!("Test passed!")
+}
 
-    value = !value;
 
-    println!("Value is {}", value);
-    println!("the value is {:08b}", value);
-
-    value = value & 0b1111_0111;
-
-    println!("Value is {}", value);
-    println!("the value is {:08b}", value);
-
-    println!("bit 6 is {}", value & 0b0100_0000);
-
-    value = value | 0b0100_0000;
-    println!("Value is {}", value);
-    println!("the value is {:08b}", value);
-
-    println!("bit 6 is {}", value & 0b0100_0000);
-
-    value = value ^ 0b0101_0101;
-    println!("Value is {}", value);
-    println!("the value is {:08b}", value);
-    
-    value = value << 4;
-    println!("Value after left shift 4 times {}", value);
-    println!("the value is {:08b}", value);
-
-    value = value >> 2;
-    println!("Value after right shift 2 times {}", value);
-    println!("the value is {:08b}", value);
-
-    value = value >> 2;
-    println!("Value after right shift 2 more times {}", value);
-    println!("the value is {:08b}", value);
-    value = value >> 2;
-    println!("Value after right shift 2 more times {}", value);
-    println!("the value is {:08b}", value);
+fn celsius_to_farenheit(temp: f64) -> f64 {
+    (temp * 1.8) + 32.0
 }
