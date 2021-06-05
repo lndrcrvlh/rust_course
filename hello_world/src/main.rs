d6810e3bbb73550bed080803f3bf57cb7eab1428
leo <lndrcrvlh@gmail.com>
2021-06-05 17:19:45 -0300

    Experiment with conditionals and random data

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -5,11 +5,17 @@ fn main() {
 
     let n1: u8 = rng.gen();
     let n2: u16 = rng.gen();
+    let n3: u8 = rng.gen();
     println!("Random u8: {}", n1);
     println!("Random u16: {}", n2);
     println!("Random u32: {}", rng.gen::<u32>());
     println!("Random i32: {}", rng.gen::<i32>());
     println!("Random float: {}", rng.gen::<f64>());
+    if n1 > n3 {
+        println!("n1 is bigger");
+    } else {
+        println!("n2 is bigger");
+    }
 }
 
 // fn main() {
@@ -19,4 +25,3 @@ fn main() {
 //         println!("x is 3");
 //     }
 // }
-
