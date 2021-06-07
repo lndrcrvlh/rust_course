9b59640a66c4ab00c25e2f57744f6b4f78cc8e40
leo <lndrcrvlh@gmail.com>
2021-06-07 11:46:32 -0300

    Solves loops exercise

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,13 +1,33 @@
 fn main() {
-    let mut matrix = [[1, 2, 3], 
-                      [4, 5, 6], 
-                      [7, 8, 9]];
-    for row in matrix.iter_mut() {
-        for num in row.iter_mut(){
-            *num += 10;
-            print!("{}\t", num);
+    let numbers = [1,9,-2,0,23,20,-7,13,37,20,56,-18,20,3];
+    let mut max: i32;
+    let mut min: i32;
+    let mut mean: f64;
+
+    max = numbers[0];
+    for number in numbers.iter() {
+        if *number as i32 > max {
+            max = *number;
         }
-        println!();
+        println!("{}",max);
     }
 
+    min = numbers[0];
+    for number in numbers.iter() {
+        if *number as i32 <= min {
+            min = *number;
+        } 
+        println!("{}",min);
+    }
+
+    mean = 0.0;
+    for number in numbers.iter() {
+        mean += *number as f64;
+    }
+    mean = mean/numbers.len() as f64;
+
+    assert_eq!(max,56);
+    assert_eq!(min,-18);
+    assert_eq!(mean,12.5);
+    println!("Tests passed!");
 }
