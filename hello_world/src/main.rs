e3f995321fc24893cb98000bb5514299ace425ec
leo <lndrcrvlh@gmail.com>
2021-06-06 22:43:20 -0300

    loop lesson

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,27 +1,13 @@
-use rand::Rng;
+fn main(){
+    let mut count = 0;
 
-fn main() {
-    let mut rng = rand::thread_rng();
+    let result = loop {
+        if count == 10{
+            break count * 10;
+        }
+        count += 1;
+        println!("{}",count);
 
-    let n1: u8 = rng.gen();
-    let n2: u16 = rng.gen();
-    let n3: u8 = rng.gen();
-    println!("Random u8: {}", n1);
-    println!("Random u16: {}", n2);
-    println!("Random u32: {}", rng.gen::<u32>());
-    println!("Random i32: {}", rng.gen::<i32>());
-    println!("Random float: {}", rng.gen::<f64>());
-    if n1 > n3 {
-        println!("n1 is bigger");
-    } else {
-        println!("n2 is bigger");
-    }
+    };
+    println!("looped, result is {}", result);
 }
-
-// fn main() {
-//     let x = 33;
-
-//     if x == 3{
-//         println!("x is 3");
-//     }
-// }
